03f469be7dd2db233b5fbdde8c71ea2dd83a9b73
bbroad25 <bbroad25@gmail.com>
2025-11-01 22:05:53 -0400

    Remove redirects and update config options

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/manifest',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a422f-0215-74a0-0ed1-6821223b7267',
-        permanent: false,
-      },
-    ];
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
